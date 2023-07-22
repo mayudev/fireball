70bd4011c848224d21d70a48b78057180cbe9504
mayudev <99561108+mayudev@users.noreply.github.com>
2023-07-22 11:49:07 +0200

    fix: deadlock

--- src/util/archive.nim
+++ src/util/archive.nim
@@ -2,7 +2,8 @@ import osproc
 import error
 
 proc extractInto*(path: string, dest: string) =
-    let tar = startProcess("/bin/tar", "", ["-xvf", path, "-C", dest])
+    let tar = startProcess("/bin/tar", args = ["-xf", path, "-C", dest])
+
     let code = tar.waitForExit()
 
     if code != 0:
